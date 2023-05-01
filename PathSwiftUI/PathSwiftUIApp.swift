43e9a1798fca6c9092edacc879006095b554bb39
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-01 16:19:52 +0500

    Initial Commit

---
+++ PathSwiftUI/PathSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathSwiftUIApp.swift
+//  PathSwiftUI
+//
+//  Created by sss on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PathSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
